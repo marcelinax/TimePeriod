417cc2d416e213760f29aad557e1fbde4e9d03ce
Marcelina Sitko <marcelina.sitko@interia.pl>
2020-11-27 01:48:27 +0100

    Initial commit

---
+++ Time_TimePeriod/Time_TimePeriod/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Time_TimePeriod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Time_TimePeriod/Time_TimePeriod/Time.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Time_TimePeriod
+{
+    public struct Time: IEquatable<Time>, IComparable<Time>
+    {
+        public byte Hours { get; }
+        public byte Minutes { get; }
+        public byte Seconds { get; }
+
+        public Time(byte hour, byte minute = 0, byte second = 0)
+        {
+            if(hour > 23) throw new ArgumentException("Incorrect argument!");
+            Hours = hour;
+            if(minute > 59) throw new ArgumentException("Incorrect argument!");
+            Minutes = minute;
+            if(second > 59) throw new ArgumentException("Incorrect argument!");
+            Seconds = second;
+        }
+
+        public Time(string time)
+        {
+            var timeSpan = TimeSpan.Parse(time);
+            Hours = Convert.ToByte(timeSpan.Hours);
+            Minutes = Convert.ToByte(timeSpan.Minutes);
+            Seconds = Convert.ToByte(timeSpan.Seconds);
+        }
+        public override string ToString()
+        {
+            return $"{Hours:00}:{Minutes:00}:{Seconds:00}";
+        }
+
+        public bool Equals(Time other)
+        {
+            if (Hours != other.Hours || Minutes != other.Minutes || Seconds != other.Seconds) return false;
+            return true;
+        }
+
+        public int CompareTo(Time other)
+        {
+            if (Hours - other.Hours != 0) return Hours - other.Hours;
+            if (Minutes - other.Minutes != 0) return Minutes - other.Minutes;
+            if (Seconds - other.Seconds != 0) return Seconds - other.Seconds;
+            return 0;
+        }
+
+        public static bool operator == (Time t1, Time t2) => t1.Equals(t2);
+        public static bool operator != (Time t1, Time t2) => !(t1 == t2);
+        public static bool operator > (Time t1, Time t2) => t1.CompareTo(t2) > 0;
+        public static bool operator >= (Time t1, Time t2) => t1.CompareTo(t2) >= 0;
+        public static bool operator < (Time t1, Time t2) => t1.CompareTo(t2) < 0;
+        public static bool operator <= (Time t1, Time t2) => t1.CompareTo(t2) <= 0;
+       
+    }
+}
