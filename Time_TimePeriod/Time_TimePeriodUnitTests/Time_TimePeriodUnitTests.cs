9d09f2c4a03a5f4c8498d85d4803a12954041380
Marcelina Sitko <marcelina.sitko@interia.pl>
2020-12-02 16:40:27 +0100

    add sceleton of unitTests

---
+++ Time_TimePeriod/Time_TimePeriodUnitTests/Time_TimePeriodUnitTests.cs
@@ -0,0 +1,43 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Time_TimePeriodUnitTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+
+    #region Constructor tests
+
+    
+
+    #endregion
+
+    #region ToStringTests
+
+    
+
+    #endregion
+
+    #region Equals
+
+    
+
+    #endregion
+
+    #region CompareTo
+
+    
+
+    #endregion
+
+    #region PlusMetod
+
+    
+
+    #endregion
+}
